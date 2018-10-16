5afd22b2fce4c09453b3942bae7acfcdf0b7b0ad
Agentman123 <joseph.s.zhang@gmail.com>
2018-10-16 09:31:08 -0400

    revscoring english test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from revscoring.features import wikitext, revision_oriented, temporal
-#from revscoring.languages import english
+from revscoring.languages import english
 from revscoring.extractors import api
-import revscoring
+from revscoring.utilities.util import read_observations
 import mwapi
 import csv
 
@@ -23,11 +23,11 @@ features = [
   # Measures the size of the change in removed words
     wikitext.revision.diff.words_removed,
   # Measures the proportional change in "badwords"
-#    english.badwords.revision.diff.match_prop_delta_sum,
+    english.badwords.revision.diff.match_prop_delta_sum,
   # Measures the proportional change in "informals"
-#    english.informals.revision.diff.match_prop_delta_sum,
+    english.informals.revision.diff.match_prop_delta_sum,
   # Measures the proportional change meaningful words
-#    english.stopwords.revision.diff.non_stopword_prop_delta_sum,
+    english.stopwords.revision.diff.non_stopword_prop_delta_sum,
   # Is the user anonymous
     revision_oriented.revision.user.is_anon,
   # Is the user a bot or a sysop
@@ -46,11 +46,18 @@ with open('datasample.csv') as csv_file:
     for row in data_csv_reader:
         if row != []:
             sample.append(row[1])
-        
+
+sampleData = []
 for revid in sample:
     revid = int(revid)
-    print("https://en.wikipedia.org/wiki/?diff={0}".format(revid))
-    print(list(api_extractor.extract(revid, features)))
+    try:
+        #print("https://en.wikipedia.org/wiki/?diff={0}".format(revid))
+        sampleRevData = list(api_extractor.extract(revid, features))
+        sampleObserv = {"rev_id": revid, "cashe": sampleRevData
+        sampleData.append(sampleObserv)
+    except:
+        print('Revision Data Not Found)
+        continue
 """
 training = fileopen('datatraining.csv')
 test = fileopen('datatest.csv')
@@ -63,20 +70,30 @@ for j in range(len(test)):
     testRevId.append(test[j])
 #print(testRevId)
 
+trainingData = []
 for revTrainId in trainingRevId:
     revTrainId = int(revTrainId)
     try:
-        print("https://en.wikipedia.org/wiki/?diff={0}".format(revTrainId))
-        print(list(api_extractor.extract(revTrainId, features)))
+        #print("https://en.wikipedia.org/wiki/?diff={0}".format(revTrainId))
+        trainingRevData = list(api_extractor.extract(revTrainId, features))
+        trainingObserv = {"rev_id": revTrainId, "cache": trainingRevData}
+        trainingData.append(trainingObserv)
     except:
         print('Revision Data Not Found')
         continue
 
+testData = []
 for revTestId in testRevId:
     revTestId = int(revTestId)
     try:
-        print("https://en.wikipedia.org/wiki/?diff={0}".format(revTestId))
-        print(list(api_extractor.extract(revTestId, features)))
+        #print("https://en.wikipedia.org/wiki/?diff={0}".format(revTestId))
+        testRevData = list(api_extractor.extract(revTestId, features))
+        testObserv = {"rev_id": revTestId, "cache": testRevData}
+        testData.append(testObserv)
     except:
         print('Revision Data Not Found')
         continue
+
+#print(sampleData)
+print(trainingData)
+print(testData)
